cbff244e94cfaa91d5dbecd1ad7716a39aeadca1
Blurgyy <gy@blurgy.xyz>
2021-02-13 18:53:56 +0800

    Fix crosshair position on HUD
    Signed-off-by: Blurgyy <gy@blurgy.xyz>

--- Source/Persian/PersianHUD.cpp
+++ Source/Persian/PersianHUD.cpp
@@ -25,11 +25,11 @@ void APersianHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition(Center.X - this->CrosshairTex->GetSizeX() * 0.5,
+										  Center.Y - this->CrosshairTex->GetSizeY() * 0.5);
 
 	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+	Canvas->DrawItem(TileItem);
 }
